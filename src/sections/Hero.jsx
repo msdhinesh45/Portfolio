2ecfa5acf2cc111650cc2139a0d540e70f2a7277
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-01 20:59:49 +0530

    Hero Section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { words } from '../constants'
 
 const Hero = () => {
+    
   return (
 
     <section id="hero" className="relative overflow-hidden" >
@@ -9,7 +11,35 @@ const Hero = () => {
         </div>
 
         <div className="hero-layout">
-        
+        {/* Left Container */}
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+            <div className="flex flex-col gap-7">
+                <div className="hero-text">
+                    <h1>Shaping
+
+                        <span className="slide">
+                            <span className="wrapper">
+                                {words.map((word)=>(
+                                    <span key={word.text} 
+                                    className="flex items-center md:gap-3
+                                    gap-1 pb-2">
+                                        <img 
+                                        src={word.imgPath} 
+                                        alt={word.text } 
+                                        className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50' />
+
+                                        <span>{word.text}</span>
+                                    </span>
+                                ))}
+                            </span>
+                        </span>
+                    </h1>
+                    <h1>into Real Projects</h1>
+                    <h1>that Deliver Results</h1>
+                </div>
+            </div>
+
+        </header>
         </div>
     </section>
   )
