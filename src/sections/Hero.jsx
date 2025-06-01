4a0d87365275ca2a903df0d4578720344925e28b
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-01 20:14:11 +0530

    Background image added

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+
+    <section id="hero" className="relative overflow-hidden" >
+        <div className="absolute top-0 left-0 z-10">
+        <img src="/public/images/bg.png" alt="Background" />
+        </div>
+
+        <div className="hero-layout">
+        
+        </div>
+    </section>
   )
 }
 
