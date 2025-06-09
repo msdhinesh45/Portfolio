8e002e98a632451003aabd23c55ea986b8a27743
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-09 22:06:34 +0530

    Project 1 image added

--- src/sections/ShowCases.jsx
+++ src/sections/ShowCases.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const ShowCases = () => {
   return (
-    <div>ShowCases</div>
+    <div id="work" className='app-showcase'>
+        <div className="w-full">
+        <div className="showcaselayout">
+
+            {/* Left */}
+            <div className="first-project-wrapper">
+                <div className="image-wrapper">
+                    <img src="/images/project1.png" alt="ryde" />
+                </div>
+            </div>
+            
+            </div>
+        </div>
+    </div>
   )
 }
 
