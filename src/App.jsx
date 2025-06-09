cf5dedc44b1b28c301c354356e1a95619d564c63
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-09 21:47:40 +0530

    Show cases imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./sections/Hero";
+import ShowCases from "./sections/ShowCases";
 
 const App = () => {
   return (
     <>
         <Hero />
+        <ShowCases />
     </>
   )
 }
---
+++ src/sections/ShowCases.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowCases = () => {
+  return (
+    <div>ShowCases</div>
+  )
+}
+
+export default ShowCases
