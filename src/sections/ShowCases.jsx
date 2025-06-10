0ca227365c49a43324b13b62dab2bc4940c0a384
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-10 22:24:31 +0530

    Project animation

--- src/sections/ShowCases.jsx
+++ src/sections/ShowCases.jsx
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+
 
 const ShowCases = () => {
-  return (
-    <div id="work" className='app-showcase'>
+    const sectionRef = useRef(null);
+    const project1Ref = useRef(null);
+    const project2Ref = useRef(null);
+    const project3Ref = useRef(null);
+
+const projects = [project1Ref.current,project2Ref.current,project3Ref.current];
+
+projects.forEach((card,index)=>{
+    gsap.fromTo(
+        card,
+        {
+            y:50,opacity:0
+        },
+        {
+            y:0,
+            opacity:1,
+            duration:2,
+            delay:0.3*(index+1),
+            scrollTrigger:{
+                trigger:card,
+                start:"top bottom-=100",
+            }
+        }
+    )
+})
+    useGSAP(()=>{
+        gsap.fromTo(sectionRef.current,
+            {opacity:0},
+            {opacity:2,duration:2.5})
+    },[]);
+
+    return (
+    <section ref={sectionRef} id="work" className='app-showcase'>
         <div className="w-full">
         <div className="showcaselayout">
 
             {/* Left */}
-            <div className="first-project-wrapper">
+            <div className="first-project-wrapper" ref={project1Ref}>
                 <div className="image-wrapper">
                     <img src="/images/project1.png" alt="ryde" />
                 </div>
@@ -20,13 +59,13 @@ const ShowCases = () => {
             {/* Right */}
 
             <div className="project-list-wrapper overflow-hidden">
-                <div className="project">
+                <div className="project" ref={project2Ref}>
                     <div className="image-wrapper bg-[#ffefdb]">
                     <img src="/images/project2.png" alt="Admin dashboard" />
                     </div>
                     <h2>Admin dashboard</h2>
                 </div>
-                <div className="project">
+                <div className="project" ref={project3Ref}>
                     <div className="image-wrapper bg-[#ffe7eb]">
                     <img src="/images/project3.png" alt="Admin dashboard" />
                     </div>
@@ -35,7 +74,7 @@ const ShowCases = () => {
             </div>
             </div>
         </div>
-    </div>
+    </section>
   )
 }
 
