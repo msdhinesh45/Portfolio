b0061c244c638f4f3d74cce191b899f1072f54bf
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-11 20:07:27 +0530

    Profile name and contact button added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className='navbar'>
+        <div className="inner">
+
+{/* Name */}
+
+            <a href="#hero" className="logo">Dhinesh Kumar | 🎓</a>
+            
+
+    <nav className="desktop">
+
+    </nav>
+
+    {/* Contact button */}
+
+    <a href="#contact" className="contact-btn group">
+        <div className="inner">
+        <span>Contact Me</span>
+        </div></a>
+        </div>
+    </header>
   )
 }
 
