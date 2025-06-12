d138f96cbde48199719ba5c45c98ba9aa4cd5d46
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-12 22:14:42 +0530

    Navbar ui completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { navLinks } from '../constants'
 
 const Navbar = () => {
+
+    const [scrolled,setScrolled] = useState(false);
+
+    useEffect(()=>{
+        const handleScroll = ()=>{
+            const isScrolled = window.scrollY>10;
+            setScrolled(true);
+        }
+
+        window.addEventListener("scroll",handleScroll);
+
+        return ()=>window.removeEventListener("scroll",handleScroll);
+    },[])
+
   return (
-    <header className='navbar'>
+    <header className={`navbar ${scrolled ? 'scrolled' : 'not-scrolled'}`}>
         <div className="inner">
 
 {/* Name */}
