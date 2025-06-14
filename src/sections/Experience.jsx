dcf76cae8e253fa8634c13d6bbd802b06127f3fb
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-14 22:17:45 +0530

    Subheading added

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -5,7 +5,7 @@ const Experience = () => {
   return (
     <section id='experience' className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
         <div className="w-full h-full md:px-20 px-5">
-        <TitleHeader />
+        <TitleHeader title="Internship Experience" sub="💼 My Carrer Overview"/>
         </div>
     </section>
   )
