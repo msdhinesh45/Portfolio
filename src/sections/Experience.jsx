37df1d95e1da3460389452276fd49c31a1be366e
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-14 22:12:36 +0530

    title header imported

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import TitleHeader from '../components/TitleHeader'
 
 const Experience = () => {
   return (
     <section id='experience' className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
-        <div className="w-full h-full md:px-20:px-5">
-            
+        <div className="w-full h-full md:px-20 px-5">
+        <TitleHeader />
         </div>
     </section>
   )
