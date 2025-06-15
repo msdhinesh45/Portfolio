7dab704b14cdb13eec29910fecbf9edcea593d6a
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-15 21:40:40 +0530

    GlowCard grid images created

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const GlowCard = ({card,children}) => {
   return (
-    <div className="card card-border timeline-card rounded-xl p-10">
+    <div className="card card-border timeline-card rounded-xl p-10 cursor-pointer">
         <div className="glow"></div>
-        
+        <div className='flex items-center gap-1 mb-5'>
+            {Array.from({length:5},(_,i)=>(
+                <img src='/images/star.png' key={i} alt='star' className='size-5' />
+            ))}
+        </div>
     </div>
   )
 }
