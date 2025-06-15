4075e04bbd9ce8298b2a3d3c88de2fa29d60629c
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-15 21:33:36 +0530

    GlowCard grid images created

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import TitleHeader from '../components/TitleHeader'
 import {expCards} from '../constants/index'
+import GlowCard from '../components/GlowCard'
 const Experience = () => {
   return (
     <section id='experience' className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
@@ -11,7 +12,13 @@ const Experience = () => {
           <div className="relative z-50 xl:space-y-32 space-y-10">
             {expCards.map((card)=>(
               <div key={card.title} className="exp-card-wrapper">
-                {card.title}
+                <div className="xl:w-2/6">
+                <GlowCard card={card}>
+                  <div>
+                    <img src={card.imgPath} alt={card.title} />
+                  </div>
+                </GlowCard>
+                </div>
               </div>
 
             ))}
