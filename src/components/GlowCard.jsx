47e4eba3eac0a0312cf208a0e35a7b8c28cb1931
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-15 21:36:45 +0530

    Card hover created

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const GlowCard = () => {
+const GlowCard = ({card,children}) => {
   return (
-    <div>GlowCard</div>
+    <div className="card card-border timeline-card rounded-xl p-10">
+        <div className="glow"></div>
+        
+    </div>
   )
 }
 
