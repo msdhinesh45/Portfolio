9515e53b2c487c26d5234e7a74d13b523bf37c6e
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-15 21:11:14 +0530

    expCards came from index.js

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import TitleHeader from '../components/TitleHeader'
-
+import {expCards} from '../constants/index'
 const Experience = () => {
   return (
     <section id='experience' className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
         <div className="w-full h-full md:px-20 px-5">
         <TitleHeader title="Internship Experience" sub="💼 My Carrer Overview"/>
+
+        <div className="mt-32 relative">
+          <div className="relative z-50 xl:space-y-32 space-y-10">
+            {expCards}
+          </div>
+        </div>
         </div>
     </section>
   )
