ea65b5d8d3d5bea1ebd9d601b66fb57009496428
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-16 22:17:33 +0530

    Mouse Function applied

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 
-const GlowCard = ({card,children}) => {
+const GlowCard = ({card,children,index}) => {
 
   const cardRefs = useRef([]);
 
@@ -10,7 +10,7 @@ const GlowCard = ({card,children}) => {
   }
 
   return (
-    <div className="card card-border timeline-card rounded-xl p-10 cursor-pointer">
+    <div ref={(el)=>(cardRefs.current[index]=el)} onMouseMove={handleMouseMove(index)} className="card card-border timeline-card rounded-xl p-10 cursor-pointer">
         <div className="glow"></div>
         <div className='flex items-center gap-1 mb-5'>
             {Array.from({length:5},(_,i)=>(
