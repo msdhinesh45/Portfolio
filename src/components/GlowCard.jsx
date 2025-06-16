d88c6e18b9700e96cf445c87c5c34af0270b559c
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-16 22:27:51 +0530

    Calc the angle from center of the card

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -11,6 +11,13 @@ const GlowCard = ({card,children,index}) => {
     const rect = card.getBoundingClientRect();
     const mouseX = e.clientX - rect.left - rect.width/2;
     const mouseY = e.clientY - rect.top - rect.height/2;
+
+
+    let angle = Math.atan2(mouseY,mouseX) * (180/Math.PI);
+
+    angle = (angle + 360)%360;
+
+    card.style.setProperty("--start", angle+60);
   }
 
   return (
