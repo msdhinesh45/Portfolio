6a8253b5892b4897d9e64c8b34f5919d8c0f393a
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-19 21:47:40 +0530

    card title class

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -28,12 +28,12 @@ const Experience = () => {
                       <div className="timeline"></div>
                       <div className="gradient-line w-1 h-full"></div>
                     </div>
-              <div className="expText flex xl:gap-20 md:gap-10 gap-5 relative z-20">
+              <div className="expText flex xl:gap-20 md: gap-10 gap-5 relative z-20">
               <div className="timeline-logo">
                 <img src={card.logoPath} alt="logo" />
               </div>
               <div>
-                <h1>{card.title}</h1>
+                <h1 className='font-semibold text-3xl'>{card.title}</h1>
               </div>
               </div>
                   </div>
