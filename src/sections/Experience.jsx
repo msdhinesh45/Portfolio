26b3d72437d1f5845598684ebc99062f928e4187
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-19 21:52:10 +0530

    card title class

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -34,6 +34,7 @@ const Experience = () => {
               </div>
               <div>
                 <h1 className='font-semibold text-3xl'>{card.title}</h1>
+                <p className="my-5 text-white-50"> 📅{card.date}</p>
               </div>
               </div>
                   </div>
