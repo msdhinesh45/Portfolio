c819eed9d3a594687496b53416403c5f6398fc39
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-20 22:56:57 +0530

    GSAP library imported for scrolling animation

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import TitleHeader from '../components/TitleHeader'
 import {expCards} from '../constants/index'
 import GlowCard from '../components/GlowCard'
+import gsap from 'gsap';
+import {useGSAP} from '@gsap/react'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'; 
 const Experience = () => {
   return (
     <section id='experience' className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
