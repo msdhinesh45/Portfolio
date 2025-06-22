543770d12e7542a10360fcdadeacb857c266f115
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-22 22:24:14 +0530

    Tech stack title and sub props

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import TitleHeader from '../components/TitleHeader'
 
 const TechStack = () => {
   return (
-    <div>TechStack</div>
+    <div id='skills' className='flex-center section-padding'>
+        <div>
+            <TitleHeader 
+            title="My Development Toolkit"
+            sub="Fueling innovation through code and creativity."
+            />
+        </div>
+    </div>
   )
 }
 
