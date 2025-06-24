4a54464219c212249ee68784df6aceba03b5d727
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-24 22:04:26 +0530

    tech stack hover animation

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -12,8 +12,10 @@ const TechStack = () => {
         <div className="tech-grid">
           {techStackIcons.map((icon)=>(
             <div key={icon.name} className='card-border tech-card overflow-hidden group xl:rounded-full rounded-lg'>
+              <div className="tech-card-animated-bg"></div>
               {icon.name}              
               </div>
+              
           ))}
         </div>
 
