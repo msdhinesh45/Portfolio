1f913f7a242121716aee2c83e18d42871aa51570
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-24 21:48:59 +0530

    Title header classname added

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -4,11 +4,15 @@ import TitleHeader from '../components/TitleHeader'
 const TechStack = () => {
   return (
     <div id='skills' className='flex-center section-padding'>
-        <div>
+        <div className='w-full h-full md:px-10 px-5'>
             <TitleHeader 
             title="My Development Toolkit"
             sub="Fueling innovation through code and creativity."
             />
+        <div className="tech-grid">
+          
+        </div>
+
         </div>
     </div>
   )
