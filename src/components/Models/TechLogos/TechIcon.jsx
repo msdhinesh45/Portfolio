b57f9915ca236879c7c072126a8fe8663865f21b
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-26 22:04:01 +0530

    Model object are called

--- src/components/Models/TechLogos/TechIcon.jsx
+++ src/components/Models/TechLogos/TechIcon.jsx
@@ -1,10 +1,20 @@
-import { useGLTF } from '@react-three/drei'
+import { Environment, Float, useGLTF } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 import React from 'react'
 
 const TechIcon = ({model}) => {
  const scene = useGLTF(model.modelPath)
   return (
-    <div></div>
+    <Canvas>
+      <ambientLight intensity={0.3} />
+      <Environment preset='city' />
+
+      <Float>
+        <group>
+          <primitive object={scene.scene}/>
+        </group>
+      </Float>
+    </Canvas>
   )
 }
 
