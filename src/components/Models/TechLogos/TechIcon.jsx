640372f8a00ad3d581fd715b0f18f33c1f15e072
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-06-27 22:20:35 +0530

    Modified

--- src/components/Models/TechLogos/TechIcon.jsx
+++ src/components/Models/TechLogos/TechIcon.jsx
@@ -9,7 +9,7 @@ const TechIcon = ({model}) => {
       <ambientLight intensity={0.3} />
       <Environment preset='city' />
 
-      <Float speed={5.5} rotationIntensity={1.7} floatIntensity={1.5}>
+      <Float speed={5.5} rotationIntensity={1.7} floatIntensity={1.5} >
         <group scale={model.scale} rotation={model.rotation}>
           <primitive object={scene.scene}/>
         </group>
