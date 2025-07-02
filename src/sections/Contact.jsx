5a654b06c3034301ca391d6ee0be2744ed94b67b
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-07-02 19:27:54 +0530

    Title added

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import TitleHeader from '../components/TitleHeader'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section id="contact" className="flex-center section-padding">
+      <div className="w-full h-full md:px-10 px-5">
+    <TitleHeader 
+    title="Get in Touch with me"
+    sub="Contact Information"
+    />        
+      </div>
+    </section>
   )
 }
 
