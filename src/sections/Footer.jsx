33f1b1a84408d12be011fedf3262cc89bc26f709
msdhinesh45 <dhineshkumar111202@gmail.com>
2025-07-04 19:53:26 +0530

    Modified

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import {socialImgs} from '../constants/index'
+import { socialImgs } from "../constants";
+
 const Footer = () => {
   return (
     <footer className="footer">
-        <div className="footer-container">
-            <div className="flex flex-col justify-center md:items-start items-center">
-                <a href="#">Visit my Portfolio</a></div>
+      <div className="footer-container">
+        <div className="flex flex-col justify-center">
+          <p>Terms & Conditions</p>
         </div>
         <div className="socials">
-            {socialImgs.map((img)=>(
-                <a className="icon" target="_blank" href={img.url} key={img.url}>
-                    <img src={img.imgPath} />
-                
-                </a>
-            ))}
+          {socialImgs.map((socialImg, index) => (
+            <div key={index} className="icon">
+              <img src={socialImg.imgPath} alt="social icon" />
+            </div>
+          ))}
         </div>
         <div className="flex flex-col justify-center">
-            <p className="text-center md:text-end">
-                &copy;; {new Date().getFullYear()} Dhinesh Kumar | Student. All rights reserved
-            </p>
+          <p className="text-center md:text-end">
+            © {new Date().getFullYear()} Dhinesh Kumar. All rights reserved.
+          </p>
         </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
